03224c63a2f0a48c0b0c417c67d630455e15e23a
Marc Hoersken <info@marc-hoersken.de>
2018-09-04 20:41:08 +0200

    Add functionality to hide environment variables from exec command

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,4 +5,8 @@ if [ -d "${DOCKER_ENTRYPOINT_DIR}" ]; then
     /bin/run-parts --verbose --regex '^[.a-zA-Z0-9_-]+$' "${DOCKER_ENTRYPOINT_DIR}"
 fi
 
+for DOCKER_ENV_HIDEVAR in ${DOCKER_ENV_HIDEVARS}; do
+    unset ${DOCKER_ENV_HIDEVAR}
+done
+
 exec "$@"
